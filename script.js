abd1204e86ac12591ff8a5a09909c117e5d866dd
James Adam <jamesadam222@hotmail.com>
2023-09-29 12:17:59 +0100

    generate rows and boxes basic logic

---
+++ script.js
@@ -0,0 +1,27 @@
+let gridSize = 4;
+const container = document.querySelector(".container");
+
+//function to create 16x16 grid
+function createGrid (numOf) {
+    //creating row
+    for (let i = 0; i < numOf; i++) {
+        const row = document.createElement('div');
+        row.classList.add('grid-row');
+
+        //inner loop. This loop must finish before going back to outer loop
+        //creating each box in row
+        for (let j = 0; j < numOf; j++) {
+            const gridBox = document.createElement('div');
+            gridBox.classList.add('grid-box');
+            //put box in row
+            row.appendChild(gridBox);
+        }
+
+        //out of outer loop
+        //append row to container
+        container.appendChild(row);
+    }
+}
+
+
+createGrid(gridSize);
